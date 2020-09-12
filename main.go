5edb5c74820ca14b3371337b6352644d345047d8
Cesar Lugo <cesarlugo00@hotmail.com>
2020-09-12 12:58:35 +0200

    refactor for readibility

--- main.go
+++ main.go
@@ -15,6 +15,11 @@ import (
 
 const maxClients = 5
 
+type counter struct {
+	uniqueNumbers 	 int
+	duplicateNumbers int
+}
+
 func main() {
 
 	li, err := net.Listen("tcp", ":4000")
@@ -24,63 +29,41 @@ func main() {
 
 	defer li.Close()
 
-	file, err := os.Create("./log.txt")
+	file, err := os.Create("./numbers.log")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	sema := make(chan struct{}, maxClients)
+
 	c := make(chan string)
 	terminate := false
 
-	go func() {
-		for {
-			sema <- struct{}{}
-			defer func() { <-sema }()
-			conn, err := li.Accept()
-			if err != nil {
-				log.Println(err)
-				// check
-				continue
-			}
+	go serveListeners(li, c, &terminate)
 
-			go handleConn(conn, c, &terminate)
-		}
-	}()
+	count := counter{uniqueNumbers: 0, duplicateNumbers: 0}
+	inputBuffer := new(bytes.Buffer)
 
-	var uniqueNumbers []string
-	numberOfDuplicates := 0
-	records := new(bytes.Buffer)
-
-	go recordInputs(c, &numberOfDuplicates, &uniqueNumbers, records)
-	printReport(&uniqueNumbers, &numberOfDuplicates, &terminate)
-
-	piper, pipew := io.Pipe()
-	go func() {
-		defer pipew.Close()
-		io.Copy(pipew, records)
-	}()
-
-	io.Copy(file, piper)
-	piper.Close()
-	
+	go trackInputs(c, &count, inputBuffer)
+	startReportLoop(&count, &terminate)
+	saveToFile(inputBuffer, file)
 }
 
-func recordInputs(c chan string, numberOfDuplicates *int, uniqueNumbers *[]string, records *bytes.Buffer) {
-	sbf := boom.NewDefaultStableBloomFilter(10000, 0.01)	
-	
+func serveListeners(li net.Listener, c chan string, terminate *bool) {
+	sema := make(chan struct{}, maxClients)	
+
 	for {
-		newValue := <-c
+		// stop serving more than max number of clients
+		sema <- struct{}{}
+		defer func() { <-sema }()
 
-		if sbf.Test([]byte(newValue)) {
-			*numberOfDuplicates++
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
 			continue
-		} 
+		}
 
-		sbf.Add([]byte(newValue))
-		*uniqueNumbers = append(*uniqueNumbers, newValue)
-		records.WriteString(newValue + "\n")
+		go handleConn(conn, c, terminate)
 	}
 }
 
@@ -109,23 +92,53 @@ func handleConn(conn net.Conn, c chan string, terminate *bool) {
 	defer conn.Close()
 }
 
-func printReport(uniqueNumbers *[]string, numberOfDuplicates *int, terminate *bool){
-	intervalCountUnique := 0
-	lastTotal := 0
-	intervalCountDuplicate := 0
+func saveToFile(inputBuffer *bytes.Buffer, file *os.File) {
+	piper, pipew := io.Pipe()
+	go func() {
+		defer pipew.Close()
+		io.Copy(pipew, inputBuffer)
+	}()
+	
+	io.Copy(file, piper)
+	piper.Close()
+	fmt.Println("Finished storing Numbers")
+}
+
+func trackInputs(c chan string, count *counter, inputBuffer *bytes.Buffer) {
+	sbf := boom.NewDefaultStableBloomFilter(10000, 0.01)	
+	
+	for {
+		newValue := <-c
+
+		if sbf.Test([]byte(newValue)) {
+			count.duplicateNumbers++
+			continue
+		} 
+
+		sbf.Add([]byte(newValue))
+		count.uniqueNumbers++
+		inputBuffer.WriteString(newValue + "\n")
+	}
+}
+
+
+
+func startReportLoop(count *counter, terminate *bool){
+	lastUniqueTotal := 0
 	lastDuplicateTotal := 0
 
 	for !*terminate {
 		time.Sleep(10000 * time.Millisecond)
-		quantity := len(*uniqueNumbers)
-		intervalCountUnique = quantity - lastTotal
-		lastTotal = quantity
 
-		duplicateTotal := *numberOfDuplicates
-		intervalCountDuplicate = duplicateTotal - lastDuplicateTotal
+		uniqueTotal := count.uniqueNumbers
+		uniqueInterval := uniqueTotal - lastUniqueTotal
+		lastUniqueTotal = uniqueTotal
+
+		duplicateTotal := count.duplicateNumbers
+		duplicateInterval := duplicateTotal - lastDuplicateTotal
 		lastDuplicateTotal = duplicateTotal
 
-		fmt.Printf("Received %v unique numbers, %v duplicates. Unique total: %v \n", intervalCountUnique, intervalCountDuplicate, quantity)
+		fmt.Printf("Received %v unique numbers, %v duplicates. Unique total: %v \n", uniqueInterval, duplicateInterval, uniqueTotal)
 	}
 }
 
